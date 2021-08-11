d6a2db4198c535b85386ae2f6a47674523c416c5
leo <xz@lgysh.cn>
2021-08-12 03:22:44 +0800

    init

---
+++ totem/src/pages/my/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <view>
+        我的
+    </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    onLoad() {},
+    methods: {},
+})
+</script>
+
+<style></style>
